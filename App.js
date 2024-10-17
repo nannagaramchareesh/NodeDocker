ab781faabd38299ed8f685613151894d13feac32
NANNAGARAM CHAREESH <22h51a05b1@cmrcet.ac.in>
2024-10-17 16:21:44 +0530

    committed

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const port=3000;
 app.get("/",(req,res)=>{
-    res.send("<h1>hello vanshika</h1>");
+    res.send("<h1>hello cherry</h1>");
 });
 app.listen(port,()=>{
     console.log(`app listening to the port ${port}`);
